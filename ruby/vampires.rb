d7eacd24798cb88d439b9f1f9e902d827141f53f
Tatiana Ab <abramovatatian@gmail.com>
2016-10-28 21:15:10 -0400

    Add statement

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,18 +5,33 @@ puts "What is your name?"
 name=gets.chomp
 
 puts "How old are you?" 
-age=gets.chomp
+age=gets.chomp.to_i
 
 puts "What year were you born?"
-year = gets.chomp
+yob = gets.chomp.to_i
+
+# If an employee gives an age and a year of birth that don’t line up
+# mathematically, that employee might be a vampire.
+
+if age = 2016 - yob
+  age = true
+end   
 
 puts "Our company cafeteria serves garlic bread. 
 Should we order some for you?"
 g_eater=gets.chomp
 
+if g_eater == "yes"
+  g_eater = true
+end  
+
 puts "Would you like to enroll in the company’s 
 health insurance? "
 ins=gets.chomp
+
+if ins=="yes"
+  ins=true
+end
 
 # If the employee got their age right, 
 # and is willing to eat garlic bread or 
@@ -29,14 +44,14 @@ if age && (g_eater || ins)
 # and hates garlic bread or waives insurance,
 # the result is “Probably a vampire.”
 
-if !age && !(g_eater || ins)
+elsif !age && !(g_eater || ins)
  puts "Probably a vampire"
 
 # If the employee got their age wrong, 
 # hates garlic bread, and doesn’t want insurance, 
 # the result is “Almost certainly a vampire.”
 
-if !age && !g_eater && !ins
+elsif !age && !g_eater && !ins
  puts "Almost certainly a vampire."
 
 # Even if the employee is an amazing liar otherwise,
@@ -44,9 +59,9 @@ if !age && !g_eater && !ins
 # is clearly a vampire, because come on. In that case, 
 # you should print “Definitely a vampire.”
 
-case name
- when "Drake Cula", "Tu Fang" 
-   puts "Definitely a vampire!"
- end  
 
 # Otherwise, print “Results inconclusive.”
+else
+  puts "Results inconclusive"
+end  
+
