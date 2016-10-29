ed7054d7ab57ab7ea66288fd65c20914a7a051e2
Tatiana Ab <abramovatatian@gmail.com>
2016-10-29 13:52:51 -0400

    Release 3: Process Multiple Employees

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,67 +1,76 @@
 # New employee file
 # Ask the new employee the following questions.
 
+def questionnaire 
 
+ puts "What is your name?"
+ emp_name=gets.chomp
 
-puts "What is your name?"
-emp_name=gets.chomp
+ puts "How old are you?" 
+ age=gets.chomp.to_i
 
-puts "How old are you?" 
-age=gets.chomp.to_i
+ puts "What year were you born?"
+ yob = gets.chomp.to_i
 
-puts "What year were you born?"
-yob = gets.chomp.to_i
+ # If an employee gives an age and a year of birth that don’t line up
+ # mathematically, that employee might be a vampire.
 
-# If an employee gives an age and a year of birth that don’t line up
-# mathematically, that employee might be a vampire.
-
-if age == 2016 - yob
+ if age == 2016 - yob
   age = true
   else
   age=false
-end   
+ end   
 
-puts "Our company cafeteria serves garlic bread. 
-Should we order some for you?"
-g_eater=gets.chomp
+ puts "Our company cafeteria serves garlic bread. 
+ Should we order some for you?"
+ g_eater=gets.chomp
 
-g_eater == "yes" ? g_eater = true: g_eater=false
+ g_eater == "yes" ? g_eater = true: g_eater=false
 
-puts "Would you like to enroll in the company’s 
-health insurance? "
-ins=gets.chomp
+ puts "Would you like to enroll in the company’s 
+ health insurance? "
+ ins=gets.chomp
 
-ins=="yes" ? ins = true: ins=false
+ ins=="yes" ? ins = true: ins=false
   
-# If the employee got their age right, 
-# and is willing to eat garlic bread or 
-# sign up for insurance, the result is “Probably not a vampire.”
+ # If the employee got their age right, 
+ # and is willing to eat garlic bread or 
+ # sign up for insurance, the result is “Probably not a vampire.”
 
-r = 'Probably not a vampire.' if age && (g_eater || ins)
+ r = 'Probably not a vampire.' if age && (g_eater || ins)
 
-# If the employee got their age wrong, 
-# and hates garlic bread or waives insurance,
-# the result is “Probably a vampire.”
+ # If the employee got their age wrong, 
+ # and hates garlic bread or waives insurance,
+ # the result is “Probably a vampire.”
 
-r = 'Probably a vampire' if !age && (!g_eater || !ins)
+ r = 'Probably a vampire' if !age && (!g_eater || !ins)
   
 # If the employee got their age wrong, 
 # hates garlic bread, and doesn’t want insurance, 
 # the result is “Almost certainly a vampire.”
 
-r = 'Almost certainly a vampire' if !age && !g_eater && !ins 
+ r = 'Almost certainly a vampire' if !age && !g_eater && !ins 
   
 # Even if the employee is an amazing liar otherwise,
 # anyone going by the name of “Drake Cula” or “Tu Fang” 
 # is clearly a vampire, because come on. In that case, 
 # you should print “Definitely a vampire.”
 
-r = "Definitely a vampire!" if emp_name == "Drake Cula" || emp_name == "Tu Fang" 
+ r = "Definitely a vampire!" if emp_name == "Drake Cula" || emp_name == "Tu Fang" 
 
-puts r
+ puts r
 
 # Otherwise, print “Results inconclusive.”
-if r 
+ if r 
  puts r
  else puts "Results inconclusive."
-end 
+ end 
+end
+
+puts "How many employees will be processed?"
+emp_number = gets.chomp.to_i
+
+until emp_number == 0
+  questionnaire
+  emp_number -= 1
+end  
