e9d8639c92900cbceb69aeadfb960f62a8aeaf71
Tatiana Ab <abramovatatian@gmail.com>
2016-11-08 14:52:25 -0500

    Add release 1 and 2

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,14 +1,10 @@
-
-first_name = gets.chomp
-last_name = gets.chomp
-
 def transform_name(name)
   name = name.downcase.chars
   name.map do |x|
     if "aeiou".include? x
-    vowel(x)
-    else
-    non_vowel(x)
+      vowel(x)
+        else
+         non_vowel(x)
     end
   end  
 end
@@ -17,8 +13,8 @@ def vowel(c)
   vowels = "aeiou"
     if c == "u"
       c.replace("a")
-    else 
-      c = vowels[vowels.index(c).next]
+        else 
+         c = vowels[vowels.index(c).next]
     end
 end    
 
@@ -26,13 +22,35 @@ def non_vowel(c)
   non_vowel = "abcdefghijklmnopqrstuvwxyz".delete("aeiou")
     if c == "z"
       c.replace("b")
-    else 
-      c = non_vowel[non_vowel.index(c).next]
+        else 
+          c = non_vowel[non_vowel.index(c).next]
     end
 end    
 
-alias_first_name = transform_name(first_name).join.capitalize
-alias_last_name = transform_name(last_name).join.capitalize
+allias_names = {}
 
-p alias_last_name + " " + alias_first_name
+first_name = ""
+last_name = ""
 
+while first_name != "quit" || last_name != "quit"
+    
+  puts "Please type first name or 'quit' to exit a program."
+  first_name =gets.chomp
+    
+    if first_name == "quit" 
+      break
+        else
+          puts "Please type last name or 'quit' to exit a program."
+          last_name=gets.chomp
+            if last_name == "quit"
+               break
+                 else
+                   alias_first_name = transform_name(first_name).join.capitalize
+                   alias_last_name = transform_name(last_name).join.capitalize
+                   p allias_full_name = alias_last_name + " " + alias_first_name
+                   allias_names[first_name + ' ' + last_name]= allias_full_name
+            end
+    end
+end
+
+allias_names.each {|k,v| puts "#{k} is also known as #{v}"}
