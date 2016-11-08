b04cb9e4babc0e2edc3e0eeb6d1974f50760ee6e
Tatiana Ab <abramovatatian@gmail.com>
2016-11-08 10:11:01 -0500

    Add commands to access nested items

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,4 +1,4 @@
-my_building = {
+building = {
 
   super: "Americo",
 
@@ -6,7 +6,7 @@ my_building = {
 
   address: "410 Ave X",
 
-  quantity_of_apt: 9,  
+  quantity_of_apt: 4,  
    
   basement: ["laundry room", "compactor room"],
 
@@ -24,7 +24,8 @@ my_building = {
         tenant_name: "Joe Brown",
         layout: "2bd",
         balcony: true 
-      },
+      }
+    },
     second_floor: {
       c: {
         tenant_name: "Jim Ritz",
@@ -42,3 +43,30 @@ my_building = {
   amenities: ["rooftop", "laundry room"] 
 }
 
+puts building[:apts_info][:second_floor][:d][:balcony]
+puts building[:amenities][1] 
+
+puts building[:apts_info][:first_floor][:a][:layout]
+puts building[:apts_info][:first_floor][:a][:layout]="3bd"
+
+building[:amenities] << "gym"
+building[:amenities].inspect
+
+building[:basement].delete_at(1)
+p building[:basement] 
+
+puts building[:apts_info][:second_floor][:c].delete(:tenant_name)
+p building[:apts_info][:second_floor][:c]
+puts building[:apts_info][:second_floor][:c][:tenant_name]=nil
+p building[:apts_info][:second_floor][:c]
+
+building[:apts_info][:second_floor].merge!({ f: {tenant_name: "Joe Doe", layout: "1Bd", balcony: false }})
+p building[:apts_info][:second_floor] 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
