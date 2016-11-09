5c952380e62166d52a5e450ade52daff421e0545
Tatiana Ab <abramovatatian@gmail.com>
2016-11-09 08:06:41 -0500

    Design a Puppy class

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+SPECIES ------------------------
+Puppy
+
+CHARACTERISTICS ----------------
+Weight: varies
+Friendly: yes
+Paw count: 4
+Name: varies
+Color: varies
+Food: puppies_food
+
+BEHAVIOR -----------------------
+Run
+Lick
+Play
+Sleep
+Bark
+
+class Puppy
+end
+
+duchess = Puppy.new
+fido = Puppy.new
+spot = Puppy.new
