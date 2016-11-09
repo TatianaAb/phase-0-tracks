70c4b324fee27124f8b08b041c3b71431c987fb4
Tatiana Ab <abramovatatian@gmail.com>
2016-11-09 11:26:45 -0500

    Add release 0

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -5,5 +5,27 @@ class Puppy
     toy
   end
 
+  def speak(int)
+    int.times {puts "Woof!"}
+  end
+
+  def roll_over
+  p "Roll over" 
+  end
+  
+  def dog_years(int)
+    int/7
+  end
+
+  def yawn
+    p "Yawn"
+  end
+
 end
 
+spot = Puppy.new
+
+spot.speak(2)
+spot.yawn
+spot.fetch("toy")
+spot.dog_years(45)
