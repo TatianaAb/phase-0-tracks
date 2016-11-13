c0035df96d179f576ab97dfd90ba76a2d6234e4e
Tatiana Ab <abramovatatian@gmail.com>
2016-11-13 12:45:50 -0500

    Release 4: Reflect

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,94 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # create an empty hash called 'list'
+  # split input based on items separated by spaces 
+  # set default quantity to 1
+  # print the list to the console [can you use one of your other methods here?]
+# output: [hash with item => quantity pairs]
+
+def list_creator(items)
+  list = {}
+  items.split.each do |item|
+   list[item] = 1
+  end 
+list
+end
+
+# Method to add an item to a list
+# input: takes in hash, item name and optional quantity
+# steps: 1.check if any items currently exist in a list
+# 2. If hash contains an item then update qty
+#     else add item to the list with qty. 
+# output: updated 'list' hash
+
+def add_item (list, item, qty=1)
+
+  if list[item]
+    list[item]+=qty
+  else
+    list[item]=qty
+  end   
+  list
+
+end
+
+p add_item(my_list, 'lemonade', 2)
+p add_item(my_list, 'tomato', 3)
+p add_item(my_list, 'onion', 1)
+p add_item(my_list, 'ice cream', 4)
+
+# Method to remove an item from the list
+# input: take 'list' as a hash an item as a string
+# steps: delete identified item
+# output: print updated 'list' hash
+
+def remove_item (list, item)
+   
+  list.delete(item) if list.include?(item)
+  list 
+
+end
+
+# Method to update the quantity of an item
+# input: takes 'list' as a hash, item name and qty 
+# steps: set qty to inputed qty
+# output: print updated list as hash
+
+def update_list(list, item, qty=1)
+
+  list[item]=qty
+  list
+end
+
+# Method to print a list and make it look pretty
+# input: list as hash
+# steps: iterate trough hash and print each key-value pair  
+# output: printed 'list' hash
+
+def print_list(list)
+  
+  list.each { |item, qty| puts "Buy #{qty} #{item}" }
+
+end
+
+# - What did you learn about pseudocode from working on this challenge?
+#      I've learned that pseudocode is very helpful.
+# When you wright an actual code it keeps you on right track. 
+
+# - What are the tradeoffs of using arrays and hashes for this challenge?
+#      We were using hashes as it was more convenient for key-value pairs.
+
+# - What does a method return?
+#    Method returns return value of its last statement
+
+# - What kind of things can you pass into methods as arguments? 
+#    We can pass all kind of things like arrays, hashes, integers and ranges
+
+# - How can you pass information between methods?
+#     Wrap them into same class and pass informarion using class variable. 
+
+# - What concepts were solidified in this challenge, 
+# and what concepts are still confusing?
+#     This challange was good for refresh knowledge of using methods. 
+
