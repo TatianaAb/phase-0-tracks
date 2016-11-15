f0ce884cc41a48d2c512f45aedf8bf7fab9c6b61
Tatiana Ab <abramovatatian@gmail.com>
2016-11-15 10:20:03 -0500

    Add Reflection

--- ruby/gps6/my_solution.rb
+++ ruby/gps6/my_solution.rb
@@ -22,17 +22,18 @@ private
   def predicted_deaths
     # predicted deaths is solely based on population density
     if @population_density >= 200
-      number_of_deaths = (@population * 0.4).floor
+      number_of_deaths = (@population * 0.4)
     elsif @population_density >= 150
-      number_of_deaths = (@population * 0.3).floor
+      number_of_deaths = (@population * 0.3)
     elsif @population_density >= 100
-      number_of_deaths = (@population * 0.2).floor
+      number_of_deaths = (@population * 0.2)
     elsif @population_density >= 50
-      number_of_deaths = (@population * 0.1).floor
+      number_of_deaths = (@population * 0.1)
     else
-      number_of_deaths = (@population * 0.05).floor
+      number_of_deaths = (@population * 0.05)
     end
-    print "#{@state} will lose #{number_of_deaths} people in this outbreak"
+
+    print "#{@state} will lose #{number_of_deaths.floor} people in this outbreak"
   end
   ### Calculates speed of spread per each state based on its population density.
   def speed_of_spread #in months
@@ -69,5 +70,26 @@ STATE_DATA.each do | state, state_info |
   prediction = VirusPredictor.new(state, STATE_DATA[state][:population_density], STATE_DATA[state][:population])
   prediction.virus_effects
 end
+
 #=======================================================================
 # Reflection Section
+
+# What are the differences between the two different hash syntaxes shown
+#  in the state_data file?
+#   One hash has a string keys and '=>' pointing to value and a another 
+#   has a symbol keys and ':' between key-value pair. Both syntaxes are valid but ':'
+#   can be used with symbol keys only.
+
+# What does require_relative do? How is it different from require?
+#   require_relative allows us to use data from external files indicating 
+#   path to required file. Require_relative is more convinient way then Require 
+#   which requires an absolute path (i.e. starts with / or ./ or ../).  
+
+#   use code block methods or loop
+
+# When refactoring virus_effects, what stood out to you about the variables, if anything?
+#    I was not sure why we have this method at all if we can just use 
+#    those two methods inside of it one by one
+
+# What concept did you most solidify in this challenge?
+#    Concept of using data in external files and use of classes (like private method). 
