fb2cc52990ac2b3c8b98beb50ce1cff9d06f64af
Tatiana Ab <abramovatatian@gmail.com>
2016-11-21 11:05:18 -0500

    Add psudocode

--- js/algos.js
+++ js/algos.js
@@ -1,4 +1,11 @@
 // Release 0: Find the Longest Phrase
+// Take an array and loop trough each element in it.
+// IF length of first element less then length of second,
+//  then delete first element
+//  ELSE 
+//  delete second element
+//  
+//  RETURN first element which is also the only element left in array.
 
 function longestPhrase (b){
   for (var i=1; i< b.length;) {
@@ -13,6 +20,10 @@ return  b[0]
 } 
 
 // Release 1: Find a Key-Value Match
+// Take two Objects set output var to false.
+// Loop trough each key in first object and compare it with every key in second object.
+// IF same key is found and their values are equal the set output to true.
+// RETURN output
 
 function match(first, second) {
 var output = false  
@@ -25,17 +36,19 @@ return output;
 }
 
 // Release 2: Generate Random Test Data
+Take any number.
+
 
 function CreateArray (arrayLength){
   this.arrayLength = arrayLength;
   this.newArray=[];
-  this.alphabet = "abcdefghijklmnop";
+  this.alphabet = "abcdefghijklmnopqrstuvwxyz";
   
   this.randomString = function (){
     this.string = '';
     this.stringLength = Math.floor(Math.random() * 10) + 1;
     for (i=1; i<=this.stringLength; i++){
-      this.index = Math.floor(Math.random() * 16); 
+      this.index = Math.floor(Math.random() * 26); 
       this.string += this.alphabet[this.index];
   }
   return this.string;
@@ -48,30 +61,16 @@ function CreateArray (arrayLength){
 return this.newArray;
 };
 }
-
-function longestPhrase (b){
-  // Release 0: Find the Longest Phrase
-
-  for (var i=1; i< b.length;) {
-    if (b[0].length < b[1].length){
-      b.splice(0,1);
-    }
-    else {
-      b.splice(1,1);
-    }
-    
-  }
-return  b[0]
 } 
 
 
 // Driver Code for Release 2 Generate Random Test Data
 
-// for (p=0; p<10; p++){
-//  var x = new CreateArray(5).randomArray();
-//  console.log(x);
-//  console.log(longestPhrase(x));
-//  }
+ for (p=0; p<10; p++){
+ var x = new CreateArray(5).randomArray();
+ console.log(x);
+ console.log(longestPhrase(x));
+ }
 
 
 // Driver Code for Release 1: Find a Key-Value Match
@@ -82,6 +81,6 @@ return  b[0]
 
 
 
-// var points = new Array(40, 100);  // Creates an array with two elements (40 and 100)
-// var points = new Array(40);       // Creates an array with 40 undefined elements !!!!!
+// new Array(40, 100);  // Creates an array with two elements (40 and 100)
+// new Array(40);       // Creates an array with 40 undefined elements !!!!!
 // Math.floor(Math.random() * 10) + 1;  // returns a number between 1 and 10
